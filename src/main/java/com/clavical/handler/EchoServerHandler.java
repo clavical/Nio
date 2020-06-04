4b7082865937d77f9f5743c41fb07872550b6659
nio4531 <453183259@qq.com>
2020-06-04 15:14:33 +0800

    first commit

---
+++ src/main/java/com/clavical/handler/EchoServerHandler.java
@@ -0,0 +1,22 @@
+package com.clavical.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    public void channelRead(ChannelHandlerContext ctx,Object msg){
+        ByteBuf in = (ByteBuf)msg;
+        System.out.println("Server receive " + in.toString(CharsetUtil.UTF_8));
+        ctx.write(in);
+    }
+
+    public void channelReadComplete(ChannelHandlerContext ctx){
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+}
